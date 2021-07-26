3feb738e11a675bf995c95d57aeeb39a6e113bc6
13952522076 <xuma@my.unt.edu>
2021-07-26 03:50:29 -0400

    update

--- pair/redering/rendering.py
+++ pair/redering/rendering.py
@@ -31,10 +31,8 @@ def main(args):
     # target = torch.from_numpy(skimage.io.imread('imgs/lena.png')).to(torch.float32) / 255.0
     img_data = skimage.io.imread(args.target)
     if img_data.shape[2]==4:
-        print("alpha channel")
-        img_data = img_data[:,:,:3]
-    else:
-        print("no alpha channel")
+        print("Input image includes alpha channel, simply dropout alpha channel.")
+        img_data = img_data[:, :, :3]
     target = torch.from_numpy(img_data).to(torch.float32) / 255.0
     target = target.pow(gamma)
     target = target.to(pydiffvg.get_device())
