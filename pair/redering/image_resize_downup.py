f18d0ddd3cd8c66f80fb93b5f80ef8b733ed72b3
13952522076 <xuma@my.unt.edu>
2021-07-31 20:27:05 -0400

    update

--- pair/redering/image_resize_downup.py
+++ pair/redering/image_resize_downup.py
@@ -11,10 +11,17 @@ if not os.path.exists(saved_dir):
 
 for s in os.listdir(dir):
     if s.endswith("jpg") or s.endswith("jpeg") or s.endswith("png"):
-        filename = s.split('.')[0]
+        filename = s.split('.')[0]   # 63-1
+        base_name = filename.split('-')[0] # 63
         src = cv2.imread(os.path.join(dir, s), cv2.IMREAD_UNCHANGED)
-        height, width = src.shape[0], src.shape[1]
+        base_src = cv2.imread(os.path.join(dir, base_name+'-1.png'), cv2.IMREAD_UNCHANGED)
+        height, width = base_src.shape[0], base_src.shape[1]
         print(f"input image: {s} [width: {width}, height: {height}]")
+
+        dsize = (width, height)
+        output = cv2.resize(src, dsize)
+        save_file_name = filename +  "-recovered.png"
+        cv2.imwrite(os.path.join(saved_dir, save_file_name), output)
 
         # for r in resize_factors:
         #     dsize = (int(width//r), int(height//r))
