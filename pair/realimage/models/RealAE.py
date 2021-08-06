c586162f691f716efe9eadcdd25f131b167afaa0
13952522076 <xuma@my.unt.edu>
2021-08-05 20:03:54 -0400

    update

--- pair/realimage/models/RealAE.py
+++ pair/realimage/models/RealAE.py
@@ -140,7 +140,7 @@ class RealAE(nn.Module):
         b, _, _, _ = x.size()
         z= self.encoder(x)
         predict = self.predictor(z)  # ["points" 2paths(3segments), "widths" paths, "colors" 4paths]
-        predict_points = (predict["points"]).view(b, self.paths, self.segments*3, 2) * self.imsize
+        predict_points = (predict["points"]).view(b, self.paths, self.segments*3, 2)
         predict_widths = (predict["widths"]).view(b, self.paths)
         predict_colors = (predict["colors"]).view(b, self.paths, 4)
         shapes_batch, shape_groups_batch = self.get_batch_shapes_groups(predict_points, predict_widths, predict_colors)
