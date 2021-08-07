d7a5fdb23d3b80e05204734e65d806472fd55f9f
13952522076 <xuma@my.unt.edu>
2021-08-07 19:54:28 -0400

    update

--- pair/optimization/rendering.py
+++ pair/optimization/rendering.py
@@ -48,20 +48,21 @@ def main(args):
         num_segments = args.num_segments
         num_control_points = torch.zeros(num_segments, dtype=torch.int32) + 2
         points = []
-        # p0 = (random.random(), random.random())
-        # points.append(p0)
-        # for j in range(num_segments):
-        #     radius = 0.05
-        #     p1 = (p0[0] + radius * (random.random() - 0.5), p0[1] + radius * (random.random() - 0.5))
-        #     p2 = (p1[0] + radius * (random.random() - 0.5), p1[1] + radius * (random.random() - 0.5))
-        #     p3 = (p2[0] + radius * (random.random() - 0.5), p2[1] + radius * (random.random() - 0.5))
-        #     points.append(p1)
-        #     points.append(p2)
-        #     if j < num_segments - 1:
-        #         points.append(p3)
-        #         p0 = p3
-        # points = torch.tensor(points)
-        points = torch.rand([num_segments*3, 2])
+        p0 = (random.random(), random.random())
+        points.append(p0)
+        for j in range(num_segments):
+            radius = 0.05
+            p1 = (p0[0] + radius * (random.random() - 0.5), p0[1] + radius * (random.random() - 0.5))
+            p2 = (p1[0] + radius * (random.random() - 0.5), p1[1] + radius * (random.random() - 0.5))
+            p3 = (p2[0] + radius * (random.random() - 0.5), p2[1] + radius * (random.random() - 0.5))
+            points.append(p1)
+            points.append(p2)
+            if j < num_segments - 1:
+                points.append(p3)
+                p0 = p3
+        points = torch.tensor(points)
+        print(f"points value is: {points}")
+        # points = torch.rand([num_segments*3, 2])  # instead from p0=   to points = torch.tensor(points)
         points[:, 0] *= canvas_width
         points[:, 1] *= canvas_height
         path = pydiffvg.Path(num_control_points=num_control_points,
