0029dded4714788a482375ebffbf6e3fa89c0993
Xu Ma <xuma@my.unt.edu>
2021-08-12 22:57:36 -0400

    Update myopt.py

--- pair/optimization/myopt.py
+++ pair/optimization/myopt.py
@@ -40,36 +40,65 @@ def main(args):
     shapes = []
     shape_groups = []
 
-    points_vars = torch.nn.Parameter(
-        0.05*(torch.rand([num_paths, args.num_segments*3, 2])-0.5) + torch.rand([num_paths, 1, 2])
-    )
-    color_vars = torch.nn.Parameter(torch.rand([num_paths,4]))
+    points_vars = []
+    color_vars = []
 
     for i in range(num_paths):
         num_segments = args.num_segments
         num_control_points = torch.zeros(num_segments, dtype=torch.int32) + 2
-        points = points_vars[i]
+        # points = []
+        #
+        # # change the detailed initlization to  will increase the loss from 0.0008 to 0.0017
+        #
+        # p0 = (random.random(), random.random())
+        # points.append(p0)
+        # for j in range(num_segments):
+        #     radius = 0.05
+        #     p1 = (p0[0] + radius * (random.random() - 0.5), p0[1] + radius * (random.random() - 0.5))
+        #     p2 = (p1[0] + radius * (random.random() - 0.5), p1[1] + radius * (random.random() - 0.5))
+        #     p3 = (p2[0] + radius * (random.random() - 0.5), p2[1] + radius * (random.random() - 0.5))
+        #     points.append(p1)
+        #     points.append(p2)
+        #     if j < num_segments - 1:
+        #         points.append(p3)
+        #         p0 = p3
+        # points = torch.tensor(points)
 
+        # print(f"points value is: {points}")  # 0.00072
+        # points = torch.rand([num_segments*3, 2])  # instead from p0=   to points = torch.tensor(points), 0.0016
+        points = 0.05*(torch.rand([num_segments*3, 2])-0.5) + torch.rand([1, 2])  # 0.00078
 
-        scaled_points = points*canvas_width
-        scaled_points = points
-        # points[:, 0] = points[:, 0] * canvas_width
-        # points[:, 1] = points[:, 1] * canvas_height
+        points[:, 0] *= canvas_width
+        points[:, 1] *= canvas_height
+        points = torch.nn.Parameter(points)
+        points_vars.append(points)
         path = pydiffvg.Path(num_control_points=num_control_points,
-                             points=scaled_points,
+                             points=points,
                              stroke_width=torch.tensor(1.0),
                              is_closed=True)
         shapes.append(path)
-        colors = color_vars[i]
+        colors = torch.nn.Parameter(torch.rand(4))
         path_group = pydiffvg.ShapeGroup(shape_ids=torch.tensor([len(shapes) - 1]),
                                          fill_color=colors)
+        color_vars.append(colors)
         shape_groups.append(path_group)
 
 
 
+
+    # for path in shapes:
+    #     path.points.requires_grad = True
+    #     points_vars.append(path.points)
+    # for group in shape_groups:
+    #     group.fill_color.requires_grad = True
+    #     color_vars.append(group.fill_color)
+
+
     # Optimize
-    points_optim = torch.optim.Adam([points_vars], lr=1.0)
-    color_optim = torch.optim.Adam([color_vars], lr=0.01)
+    points_optim = torch.optim.Adam(points_vars, lr=1.0)
+    color_optim = torch.optim.Adam(color_vars, lr=0.01)
+    # points_optim = torch.optim.SGD(points_vars, lr=1.0)
+    # color_optim = torch.optim.SGD(color_vars, lr=1.0)
     # Adam iterations.
     for t in range(args.num_iter):
         points_optim.zero_grad()
@@ -105,14 +134,17 @@ def main(args):
         # Take a gradient descent step.
         points_optim.step()
         color_optim.step()
-
-        # for group in shape_groups:
-        #     group.fill_color.data.clamp_(0.0, 1.0)
-
+        if args.use_blob:
+            for group in shape_groups:
+                group.fill_color.data.clamp_(0.0, 1.0)
+        else:
+            for group in shape_groups:
+                group.stroke_color.data.clamp_(0.0, 1.0)
 
         # if t % 10 == 0 or t == args.num_iter - 1:
         if t == args.num_iter - 1:
-            pydiffvg.save_svg('results/closed_iter_{}.svg'.format(t),
+            use_blob = "closed" if args.use_blob else "open"
+            pydiffvg.save_svg('results/{}_iter_{}.svg'.format(use_blob,t),
                               canvas_width, canvas_height, shapes, shape_groups)
 
 
