44cbcff23d9d2451680a046e81ed697c0546625f
Xu Ma <xuma@my.unt.edu>
2021-08-16 13:46:25 -0400

    add se-resnet backbone

--- pair/Reconstruction/models/__init__.py
+++ pair/Reconstruction/models/__init__.py
@@ -3,3 +3,5 @@ from .ResNetAE import ResNetAE
 from .ResNetAE2 import ResNetAE2
 from .ResNetAE3 import ResNetAE3
 from .ResNetAE4 import ResNetAE4
+from .SEResNet50AE import SEResNet50AE
+from .SEResNet101AE import SEResNet101AE
--- pair/Reconstruction/models/backbone/__init__.py
+++ pair/Reconstruction/models/backbone/__init__.py
@@ -1,2 +1,3 @@
 from __future__ import absolute_import
 from .resnet import old_resnet50, old_resnet34, old_resnet18
+from .se_resnet import se_resnet18, se_resnet34, se_resnet50, se_resnet101
