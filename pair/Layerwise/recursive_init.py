6996dcb88070fd1692071fba1228702f185c2ae8
Xu Ma <xuma@my.unt.edu>
2021-09-10 21:53:56 -0400

    Update recursive_init.py

--- pair/Layerwise/recursive_init.py
+++ pair/Layerwise/recursive_init.py
@@ -203,7 +203,7 @@ def main():
                     format(filename, args.num_paths,current_path_str[:-1], args.num_segments)
                 if args.free:
                     save_name+='-free'
-                save_name+='-init.svg'
+                save_name+='-result.svg'
                 pydiffvg.save_svg(save_name, canvas_width, canvas_height, shapes, shape_groups)
 
 
