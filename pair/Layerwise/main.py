341d514fc48a3d5ff011ea86d64ab11512ddb6a1
Xu Ma <xuma@my.unt.edu>
2021-09-22 22:21:56 -0400

    update

--- pair/Layerwise/main.py
+++ pair/Layerwise/main.py
@@ -2,7 +2,7 @@
 python main.py demo.png --num_paths 1,1,1,1 --save_loss --save_init --pool_size 12 --save_folder debug --free
 
 
-python main.py demo.png --num_paths 1,1 --pool_size 40 --save_folder video --free --save_video
+python main.py demo.png --num_paths 1,1 --pool_size 40 --save_folder video --free --save_video --num_segments 8
 """
 import pydiffvg
 import torch
@@ -277,7 +277,7 @@ def main():
             for ii in range(0, args.num_iter):
                 filename = os.path.join(save_path, "images", f"{current_path_str[:-1]}-{ii}.png")
                 img = cv2.imread(filename)
-                cv2.putText(img, f"Path:{current_path_str[:-1]} | Iteration:{ii}", (10,10),
+                cv2.putText(img, f"Path:{current_path_str[:-1]} \nIteration:{ii}", (10,10),
                     cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),  1)
                 img_array.append(img)
             videoname = os.path.join(save_path, "videos", f"{current_path_str[:-1]}.mp4")
