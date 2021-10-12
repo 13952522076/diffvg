0fb2082a868144ff980794ad1de3f7226abe1d59
Xu Ma <xuma@my.unt.edu>
2021-10-11 21:32:02 -0400

    Update background.py

--- pair/Layerwise/background.py
+++ pair/Layerwise/background.py
@@ -303,7 +303,7 @@ def main():
             if t == args.num_iter - 1:
                 save_name = os.path.join(save_path, f"{current_path_str[:-1]}.svg")
                 # pydiffvg.save_svg(save_name, canvas_width, canvas_height, shapes, shape_groups)
-                pydiffvg.save_svg(save_name, canvas_width, canvas_height, shapes, shape_groups, background="rgba(255, 0, 0, 0.3)")
+                pydiffvg.save_svg(save_name, canvas_width, canvas_height, shapes, shape_groups, background="background: rgba(255, 0, 0, 0.3)")
 
         loss_matrix.append(loss_list)
         old_shapes = shapes
