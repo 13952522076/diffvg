b7921eda92b32e278eb0e53cc89ab9bbfd0fedd7
Xu Ma <xuma@my.unt.edu>
2021-10-27 19:12:37 -0400

    Update XingLoss.py

--- pair/Layerwise/XingLoss.py
+++ pair/Layerwise/XingLoss.py
@@ -26,17 +26,17 @@ def xing_loss(x_list, scale=1.0):  # x[ npoints,2]
         Area_CD_A = triangle_area(mutual_segments[:, :, :, 2], mutual_segments[:, :, :, 3], mutual_segments[:, :, :, 0])
         Area_CD_B = triangle_area(mutual_segments[:, :, :, 2], mutual_segments[:, :, :, 3], mutual_segments[:, :, :, 1])
 
-        condition1 = ((Area_AB_C * Area_AB_D) <= 1e-5).float()
-        condition2 = ((Area_CD_A * Area_CD_B) <= 1e-5).float()
+        condition1 = ((Area_AB_C * Area_AB_D) <= 0.).float()
+        condition2 = ((Area_CD_A * Area_CD_B) <= 0.).float()
         mask = condition1*condition2
-        area_AB_1 = (abs(Area_AB_C)+ 1e-5)/(abs(Area_AB_D)+ 1e-5)
-        area_AB_2 = (abs(Area_AB_D)+ 1e-5)/(abs(Area_AB_C)+ 1e-5)
+        area_AB_1 = (abs(Area_AB_C))/(abs(Area_AB_D)+ 1e-5)
+        area_AB_2 = (abs(Area_AB_D))/(abs(Area_AB_C)+ 1e-5)
         area_AB,_ = torch.cat([area_AB_1.unsqueeze(dim=-1),area_AB_2.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
         area_AB = torch.clip(area_AB, 0.0, 1.0)
         area_AB = torch.nan_to_num(area_AB, nan=0.0)
 
-        area_CD_1 = (abs(Area_CD_A)+ 1e-5)/(abs(Area_CD_B)+ 1e-5)
-        area_CD_2 = (abs(Area_CD_B)+ 1e-5)/(abs(Area_CD_A)+ 1e-5)
+        area_CD_1 = (abs(Area_CD_A))/(abs(Area_CD_B)+ 1e-5)
+        area_CD_2 = (abs(Area_CD_B))/(abs(Area_CD_A)+ 1e-5)
         area_CD, _ = torch.cat([area_CD_1.unsqueeze(dim=-1),area_CD_2.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
         area_CD = torch.clip(area_CD, 0.0, 1.0)
         area_CD = torch.nan_to_num(area_CD, nan=0.0)
