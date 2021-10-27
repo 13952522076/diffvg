8902d4fc7fc90c44938d244b9b12d616b18ce8ac
Xu Ma <xuma@my.unt.edu>
2021-10-27 19:05:41 -0400

    update

--- pair/Layerwise/XingLoss.py
+++ pair/Layerwise/XingLoss.py
@@ -26,23 +26,22 @@ def xing_loss(x_list, scale=1.0):  # x[ npoints,2]
         Area_CD_A = triangle_area(mutual_segments[:, :, :, 2], mutual_segments[:, :, :, 3], mutual_segments[:, :, :, 0])
         Area_CD_B = triangle_area(mutual_segments[:, :, :, 2], mutual_segments[:, :, :, 3], mutual_segments[:, :, :, 1])
 
-        condition1 = ((Area_AB_C * Area_AB_D) < 0.).float()
-        condition2 = ((Area_CD_A * Area_CD_B) < 0.).float()
+        condition1 = ((Area_AB_C * Area_AB_D) <= 10).float()
+        condition2 = ((Area_CD_A * Area_CD_B) <= 10).float()
         mask = condition1*condition2
-        area_AB_1 = abs(Area_AB_C)/(abs(Area_AB_D)+ 1e-5)
-        area_AB_2 = abs(Area_AB_D)/(abs(Area_AB_C)+ 1e-5)
+        area_AB_1 = (abs(Area_AB_C)+10)/(abs(Area_AB_D)+ 1e-5)
+        area_AB_2 = (abs(Area_AB_D)+10)/(abs(Area_AB_C)+ 1e-5)
         area_AB,_ = torch.cat([area_AB_1.unsqueeze(dim=-1),area_AB_2.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
         area_AB = torch.clip(area_AB, 0.0, 1.0)
         area_AB = torch.nan_to_num(area_AB, nan=0.0)
 
-        area_CD_1 = abs(Area_CD_A)/(abs(Area_CD_B)+ 1e-5)
-        area_CD_2 = abs(Area_CD_B)/(abs(Area_CD_A)+ 1e-5)
+        area_CD_1 = (abs(Area_CD_A)+10)/(abs(Area_CD_B)+ 1e-5)
+        area_CD_2 = (abs(Area_CD_B)+10)/(abs(Area_CD_A)+ 1e-5)
         area_CD, _ = torch.cat([area_CD_1.unsqueeze(dim=-1),area_CD_2.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
         area_CD = torch.clip(area_CD, 0.0, 1.0)
         area_CD = torch.nan_to_num(area_CD, nan=0.0)
 
-        # area_loss, _ = torch.cat([area_AB.unsqueeze(dim=-1),area_CD.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
-        area_loss = area_AB+ area_CD
+        area_loss, _ = torch.cat([area_AB.unsqueeze(dim=-1),area_CD.unsqueeze(dim=-1)],dim=-1).min(dim=-1)
         area_loss = area_loss*mask
         area_loss = area_loss.sum()/((x.shape[0]-2)**2)
 
@@ -70,69 +69,77 @@ if __name__ == "__main__":
 
     
     """
-    a = torch.Tensor([[0, 0]])
-    b = torch.Tensor([[0, 3]])
-    c = torch.Tensor([[1, 1]])
-    d = torch.Tensor([[-1, 1]])
-    print(triangle_area(a, b, c))
-    print(triangle_area(a, b, d))
+    # a = torch.Tensor([[0, 0]])
+    # b = torch.Tensor([[0, 3]])
+    # c = torch.Tensor([[1, 1]])
+    # d = torch.Tensor([[-1, 1]])
+    # print(triangle_area(a, b, c))
+    # print(triangle_area(a, b, d))
 
 
-    a =torch.rand(3,2)
-    v,id = a.min(dim=-1)
-    print(v)
-    print(id)
+    # a =torch.rand(3,2)
+    # v,id = a.min(dim=-1)
+    # print(v)
+    # print(id)
 
-    print(f"===> test cosine similarity ===")
-    # doesn't work
-    points = torch.rand(13,2)
-    point_init = points[:1,:]
-    cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-    smi = cos(point_init, points)
-    indx = torch.argsort(smi, dim=0, descending=True)
-    points = points[indx,:]
-    print(smi)
+    # print(f"===> test cosine similarity ===")
+    # # doesn't work
+    # points = torch.rand(13,2)
+    # point_init = points[:1,:]
+    # cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    # smi = cos(point_init, points)
+    # indx = torch.argsort(smi, dim=0, descending=True)
+    # points = points[indx,:]
+    # print(smi)
 
-    print(f"===> vis cosine similarity ===")
-    points = torch.Tensor([[0.9665, 0.4407],
-        [0.9724, 0.4446],
-        [0.9603, 0.4345],
-        [0.9202, 0.4145],
-        [0.9622, 0.4459],
-        [0.9445, 0.4152],
-        [0.9545, 0.4520],
-        [0.9857, 0.4314],
-        [0.9638, 0.4654],
-        [0.9418, 0.4613],
-        [0.9435, 0.3927],
-        [0.9455, 0.3910]])
-    points = torch.rand(14,2)
-    # sort control points by cosine-limilarity.
-    point_init = points.mean(dim=0, keepdim=True)
-    import torch.nn.functional as F
-    smi = F.cosine_similarity(torch.tensor([[1.,0.]]), points-point_init, dim=1, eps=1e-6)
-    print(f"smi is {smi}")
-    indx = torch.argsort(smi, dim=0, descending=False)
-    print(points)
-    print(indx)
-    points = points[indx,:]
-    print(points)
-    smi = F.cosine_similarity(point_init, points, dim=1, eps=1e-6)
-    print(smi)
+    # print(f"===> vis cosine similarity ===")
+    # points = torch.Tensor([[0.9665, 0.4407],
+    #     [0.9724, 0.4446],
+    #     [0.9603, 0.4345],
+    #     [0.9202, 0.4145],
+    #     [0.9622, 0.4459],
+    #     [0.9445, 0.4152],
+    #     [0.9545, 0.4520],
+    #     [0.9857, 0.4314],
+    #     [0.9638, 0.4654],
+    #     [0.9418, 0.4613],
+    #     [0.9435, 0.3927],
+    #     [0.9455, 0.3910]])
+    # points = torch.rand(14,2)
+    # # sort control points by cosine-limilarity.
+    # point_init = points.mean(dim=0, keepdim=True)
+    # import torch.nn.functional as F
+    # smi = F.cosine_similarity(torch.tensor([[1.,0.]]), points-point_init, dim=1, eps=1e-6)
+    # print(f"smi is {smi}")
+    # indx = torch.argsort(smi, dim=0, descending=False)
+    # print(points)
+    # print(indx)
+    # points = points[indx,:]
+    # print(points)
+    # smi = F.cosine_similarity(point_init, points, dim=1, eps=1e-6)
+    # print(smi)
+    #
+    #
+    # x_list = []
+    # y_list = []
+    # labels = []
+    # for i in range(0, points.shape[0]):
+    #     x_list.append(points[i,0])
+    #     y_list.append(points[i,1])
+    #     labels.append(i)
+    # import matplotlib.pyplot as plt
+    # fig, ax = plt.subplots()
+    # ax.scatter(x_list, y_list)
+    # ax.scatter([point_init[0,0]], [point_init[0,1]])
+    # ax.plot(x_list, y_list)
+    # for i, txt in enumerate(labels):
+    #     ax.annotate(txt, (x_list[i], y_list[i]))
+    # plt.show()
 
+    a =torch.rand(4,1)
+    b = torch.permute(a, dims=[1,0])
+    c = torch.matmul(a,b)
+    d = torch.triu(c, diagonal=2)
+    print(c)
+    print(d)
 
-    x_list = []
-    y_list = []
-    labels = []
-    for i in range(0, points.shape[0]):
-        x_list.append(points[i,0])
-        y_list.append(points[i,1])
-        labels.append(i)
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.scatter(x_list, y_list)
-    ax.scatter([point_init[0,0]], [point_init[0,1]])
-    ax.plot(x_list, y_list)
-    for i, txt in enumerate(labels):
-        ax.annotate(txt, (x_list[i], y_list[i]))
-    plt.show()
