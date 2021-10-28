997c930dc8433601e1966cf7f6cea04d41bc1daa
Xu Ma <xuma@my.unt.edu>
2021-10-27 21:20:36 -0400

    Update XingLoss.py

--- pair/Layerwise/XingLoss.py
+++ pair/Layerwise/XingLoss.py
@@ -30,9 +30,10 @@ def xing_loss(x_list, scale=1.0):  # x[ npoints,2]
         condition2 = ((Area_CD_A * Area_CD_B) < 0.).float()
         mask = condition1*condition2  # mask is without gradient.
 
-        Tensor_X = Area_AB_C*Area_AB_D
-        print(f"TensorX max: {Tensor_X.max()}, min: {Tensor_X.min()}")
-        Tensor_Y = Area_CD_A*Area_CD_B
+        Tensor_X = Area_AB_C/(Area_AB_D + 1e-3)
+        Tensor_X =torch.clip(Tensor_X,-1,1)
+        Tensor_Y = Area_CD_A/(Area_CD_B+ 1e-3)
+        Tensor_Y =torch.clip(Tensor_Y,-1,1)
         angel = torch.atan2(Tensor_X+ 1e-5,Tensor_Y)
         angel = -angel-1.0
         angel_loss = angel*mask
