0a1df075dfeaa59b0c7e7a19a65cf1c9a6d706b3
Xu Ma <xuma@my.unt.edu>
2021-11-01 03:28:56 -0400

    update

---
+++ pair/generate/generate_pair.py
@@ -0,0 +1,19 @@
+"""
+"""
+import pydiffvg
+import torch
+import os
+import numpy as np
+import cv2
+import skimage
+import skimage.io
+import matplotlib.pyplot as plt
+import random
+import argparse
+import math
+import errno
+from tqdm import tqdm
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.nn.functional import adaptive_avg_pool2d
+import warnings
+warnings.filterwarnings("ignore")
---
+++ pair/generate/generate_template.py
@@ -0,0 +1,36 @@
+"""
+"""
+import pydiffvg
+import torch
+import os
+import numpy as np
+import cv2
+import skimage
+import skimage.io
+import matplotlib.pyplot as plt
+import random
+import argparse
+import math
+import errno
+from tqdm import tqdm
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.nn.functional import adaptive_avg_pool2d
+import warnings
+warnings.filterwarnings("ignore")
+pydiffvg.set_print_timing(False)
+gamma = 1.0
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--template_path", type=str, default="../data/generate/template")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    files = []
+    for file in os.listdir(args.template_path):
+        print(file)
+
+
+if __name__ == "__main__":
+    main()
