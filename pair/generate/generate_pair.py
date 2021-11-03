2f422b580c4431762f67cde3614e3320d4670734
Xu Ma <xuma@my.unt.edu>
2021-11-03 03:37:50 -0400

    Update generate_pair.py

--- pair/generate/generate_pair.py
+++ pair/generate/generate_pair.py
@@ -303,6 +303,7 @@ def detail_method(old_shapes, old_shape_groups, pixelwise_loss, num_segment, col
     # calculate regional loss_weight
     region_loss = adaptive_avg_pool2d(pixelwise_loss, args.pool_size)
     loss_weight = torch.softmax(region_loss.reshape(1,1,-1),dim=-1).reshape_as(region_loss)
+    print(f"loss_weight shape is {loss_weight.shape}")
     loss_weight = torch.nn.functional.interpolate(loss_weight, size=[canvas_height,canvas_width], mode='area')
     loss_weight = loss_weight/(loss_weight.sum())
     loss_weight = loss_weight.clone().detach()
