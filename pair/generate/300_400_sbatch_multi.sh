39f4444fba07ab85d7bde96a1e405ca2f35e271a
Xu Ma <xuma@my.unt.edu>
2021-11-04 05:42:56 -0400

    update

---
+++ pair/generate/300_400_sbatch_multi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL,BEGIN
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p multigpu
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32Gb
+#SBATCH --time=1-00:00:00
+#SBATCH --output=%j.log
+
+module load cuda/11.0
+module unload gcc/5.5.0
+module load gcc/8.1.0
+export CC=/shared/centos7/gcc/8.1.0/bin/gcc
+export CXX=/shared/centos7/gcc/8.1.0/bin/g++
+source activate im2vec
+
+cd /scratch/ma.xu1/diffvg/pair/generate
+python generate_pair.py  --pool_size 60  --free --start 300 --end 400
---
+++ pair/generate/500_1000_sbatch_multi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ma.xu1@northeastern.edu
+#SBATCH -N 1
+#SBATCH -p fugpu
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --output=%j.log
+
+module load cuda/11.0
+module unload gcc/5.5.0
+module load gcc/8.1.0
+export CC=/shared/centos7/gcc/8.1.0/bin/gcc
+export CXX=/shared/centos7/gcc/8.1.0/bin/g++
+source activate im2vec
+
+cd /scratch/ma.xu1/diffvg/pair/generate
+python generate_pair.py  --pool_size 60  --free --start 500 --end 1000
