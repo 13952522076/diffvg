1492057e4b711ac70c0835dcb3a9c0b1dc6fa437
Xu Ma <xuma@my.unt.edu>
2021-11-05 16:30:17 -0400

    Update resnet_se.py

--- pair/generate/resnet_se.py
+++ pair/generate/resnet_se.py
@@ -122,7 +122,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.lastpool = nn.AdaptiveMaxPool2d((1, 1))
+        self.lastpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc_segnum = nn.Linear(512 * block.expansion, 1)
         self.fc_color = nn.Linear(512 * block.expansion, 1)
 
