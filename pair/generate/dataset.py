3b8bb4d2a4a5aef40db3bd865b77843a8c145d85
Xu Ma <xuma@my.unt.edu>
2021-11-05 04:44:18 -0400

    update

--- pair/generate/dataset.py
+++ pair/generate/dataset.py
@@ -3,9 +3,10 @@ import pickle
 import glob
 import numpy as np
 from torch.utils.data import Dataset
+from typing import Any, Callable, Optional, Tuple
 
 
-def load_data(root="../data/generate/generate/row_data"):
+def load_data(root="../data/generate/generate/row_data/train", normalize=True):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_DIR = os.path.join(BASE_DIR, 'data')
     all_loss = []
@@ -16,7 +17,13 @@ def load_data(root="../data/generate/generate/row_data"):
         with open(file_name, "rb") as f:
             row_list = pickle.load(f)
             for row in row_list:
-                all_loss.append(row["pixelwise_loss"].astype('float32'))
+                data = row["pixelwise_loss"].astype('float32')
+                if normalize:
+                    data = (data-data.mean())/(data.std() + 1e-8)  # mean/std normalize
+                    data -= data.min()
+                    data /= (data.max() + 1e-8) # scale to [0, 1]
+
+                all_loss.append(data)
                 all_segnum.append(row["best_num_segments"]-3) # minus 3 because it starts from 3
                 all_color.append(color_map[row["best_color"]])
 
@@ -28,22 +35,34 @@ def load_data(root="../data/generate/generate/row_data"):
 
 
 class LossDataset(Dataset):
-    def __init__(self, root="../data/generate/generate/row_data"):
-        self.data, self.segnum, self.color = load_data(root)
+    def __init__(self, root="../data/generate/generate/row_data/train", normalize=True, transform: Optional[Callable] = None):
+        self.data, self.segnum, self.color = load_data(root, normalize)
+        self.transform = transform
 
     def __getitem__(self, item):
         data = self.data[item]
-        print(data.shape)
+        data = data.transpose(1, 2, 0)
         segnum = self.segnum[item]
         color = self.color[item]
+        if self.transform is not None:
+            data = self.transform(data)
         return data, segnum, color
 
     def __len__(self):
         return self.data.shape[0]
 
 if __name__ == "__main__":
+    import torchvision.transforms as transforms
     load_data()
-    dataset = LossDataset()
-    print(dataset.__len__())
-    print(dataset.__getitem__(28))
+    transform_train = transforms.Compose([ transforms.ToPILImage(),
+        transforms.Resize(224), transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+    dataset = LossDataset(transform=transform_train)
+
+    # print(dataset.__len__())
+    # print(dataset.__getitem__(28))
+    for i in range (1,500):
+        t = np.random.randint(0,300)
+        data, segnum, color = dataset.__getitem__(t)
+        # print(data.max(), data.min(), segnum, color)
+    print(data.mean())
 
---
+++ pair/generate/train.py
@@ -0,0 +1,248 @@
+'''Train LossData with PyTorch.
+e.g.
+    python3 train.py
+'''
+from __future__ import print_function
+
+import torch
+import shutil
+import logging
+import datetime
+import subprocess
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from resnet_se import se_resnet18
+import torchvision
+from dataset import LossDataset
+import torchvision.transforms as transforms
+from torch.optim.lr_scheduler import CosineAnnealingLR
+import os
+import argparse
+import warnings
+warnings.filterwarnings("ignore")
+
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--root', default="../data/generate/generate/row_data", type=str)
+    parser.add_argument('--checkpoint', type=str)
+    parser.add_argument('--bs', default=32, type=int, help='batch size')
+    parser.add_argument('--workers', default=8, type=int, help='batch size')
+    parser.add_argument('--epoch', default=300, type=int, help='epoch size')
+    parser.add_argument('--normalize', type=bool,  default=True, help='normalize input data')
+    return parser.parse_args()
+
+
+def get_git_commit_id():
+    try:
+        cmd_out = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+        return cmd_out
+    except:
+        # indicating no git found.
+        return "0000000"
+
+
+def save_model(net, epoch, path, acc, is_best, **kwargs):
+    state = {
+        'net': net.state_dict(),
+        'epoch': epoch,
+        'acc': acc
+    }
+    for key, value in kwargs.items():
+        state[key] = value
+    filepath = os.path.join(path, "last_checkpoint.pth")
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(path, 'best_checkpoint.pth'))
+
+
+def main():
+    args = parse_args()
+    os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    time_str = str(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+    args.checkpoint = 'checkpoints/' + time_str
+    if not os.path.isdir(args.checkpoint):
+        try:
+            os.makedirs(args.checkpoint)
+        except OSError as exc:  # Python >2.5
+            pass
+    # logger file
+    screen_logger = logging.getLogger("Model")
+    screen_logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(message)s')
+    file_handler = logging.FileHandler(os.path.join(args.checkpoint, "log.txt"))
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    screen_logger.addHandler(file_handler)
+
+    def printf(str):
+        screen_logger.info(str)
+        print(str)
+
+    printf(f"args: {args}")
+    printf(f"\n=== Current git ID is: {get_git_commit_id()} ===\n")
+
+    # Model
+    printf('==> Building model, criterion, optimizer..')
+    net = se_resnet18().to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+    scheduler = CosineAnnealingLR(optimizer, args.epoch, eta_min=args.lr / 1000)
+    best_test_acc = 0.  # best test accuracy
+    best_train_acc = 0.
+    best_test_loss = float("inf")
+    best_train_loss = float("inf")
+    start_epoch = 0
+
+    # Data
+    printf('==> Preparing data..')
+    # Data
+    transform_train = transforms.Compose([ transforms.ToPILImage(), transforms.Resize(224),
+                                           transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+    transform_test = transforms.Compose([ transforms.ToPILImage(), transforms.Resize(224), transforms.ToTensor() ])
+    trainset = LossDataset(root=os.path.join(args.root, "train"),  normalize=args.normalize, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.bs, shuffle=True, num_workers=args.workers)
+    testset = LossDataset(root=os.path.join(args.root, "test"),  normalize=args.normalize, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.bs, shuffle=False, num_workers=args.workers)
+
+    for epoch in range(start_epoch, args.epoch):
+        printf('Epoch(%d/%s) Learning Rate %s:' % (epoch + 1, args.epoch, optimizer.param_groups[0]['lr']))
+        # {"loss", "loss_segnum", "loss_color", "acc_segnum", "acc_color", "time"}
+        train_out = train(net, train_loader, optimizer, criterion, device)
+        test_out = validate(net, test_loader, criterion, device)
+        scheduler.step()
+        if 0.5*(test_out["acc_color"] + test_out["acc_segnum"]) > best_test_acc:
+            best_test_acc = 0.5*(test_out["acc_color"] + test_out["acc_segnum"])
+            is_best = True
+        else:
+            is_best = False
+            {"loss", "loss_segnum", "acc_segnum", "acc_color", "time"}
+        printf(f'Training loss: {train_out["loss"]}, loss_segnum: {train_out["loss_segnum"]}, '
+               f'loss_color: {train_out["loss_color"]},'
+               f'acc_segnum: {train_out["acc_segnum"]}, acc_color: {train_out["acc_color"]}')
+        printf(f'Testing  loss: {test_out["loss"]}, loss_segnum: {test_out["loss_segnum"]}, '
+               f'loss_color: {test_out["loss_color"]}, acc_segnum: {test_out["acc_segnum"]},'
+               f' acc_color: {test_out["acc_color"]}, best_meanAcc_test: {best_test_acc}')
+        save_model(
+            net, epoch, path=args.checkpoint, acc=test_out["acc_color"] + test_out["acc_segnum"],
+            is_best=is_best, best_test_acc=best_test_acc,  # best test accuracy
+            acc_color = test_out["acc_color"],
+            acc_segnum = test_out["acc_segnum"],
+
+            optimizer=optimizer.state_dict()
+        )
+
+
+def train(net, trainloader, optimizer, criterion, device):
+    net.train()
+    train_loss = 0
+    train_loss_segnum = 0
+    train_loss_color = 0
+    correct_segnum = 0
+    correct_color = 0
+    total = 0
+    time_cost = datetime.datetime.now()
+    t_range = tqdm(trainloader)
+    for batch_idx, (data, label_segnum, label_color) in enumerate(t_range):
+        data, label_segnum, label_color  = data.to(device), label_segnum.to(device), label_color.to(device)
+        logits_segnum, logits_color = net(data)
+
+        loss_segnum = criterion(logits_segnum, label_segnum)
+        loss_color = criterion(logits_color, label_color)
+        loss = loss_segnum + loss_color
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        train_loss_segnum += loss_segnum.item()
+        train_loss_color +=loss_color.item()
+        preds_segnum = logits_segnum.max(dim=1)[1]
+        preds_color = logits_color.max(dim=1)[1]
+
+        total += label_segnum.size(0)
+        correct_segnum += preds_segnum.eq(label_segnum).sum().item()
+        correct_color += preds_color.eq(label_color).sum().item()
+
+        t_range.set_postfix({'Train loss':(train_loss / (batch_idx + 1)),
+                             "acc_segnum": correct_segnum/total,
+                             "acc_color": correct_color/total,
+                             })
+    time_cost = int((datetime.datetime.now() - time_cost).total_seconds())
+    print(f"preds_segnum is: {preds_segnum}")
+    print(f"label_segnum is: {label_segnum}")
+    print(f"preds_color is: {preds_color}")
+    print(f"label_color is: {label_color}")
+    return {
+        "loss": float("%.3f" % (train_loss / (batch_idx + 1))),
+        "loss_segnum": float("%.3f" % (train_loss_segnum / (batch_idx + 1))),
+        "loss_color": float("%.3f" % (train_loss_color / (batch_idx + 1))),
+        "acc_segnum": float("%.3f" % (100. * correct_segnum/total)),
+        "acc_color": float("%.3f" % (100. * correct_color/total)),
+        "time": time_cost
+    }
+
+
+def validate(net, valloader, criterion, device):
+    net.eval()
+    val_loss = 0
+    val_loss_segnum = 0
+    val_loss_color = 0
+    correct_segnum = 0
+    correct_color = 0
+    total = 0
+    time_cost = datetime.datetime.now()
+    t_range = tqdm(valloader)
+    with torch.no_grad():
+        for batch_idx, (data, label_segnum, label_color) in enumerate(t_range):
+            data, label_segnum, label_color  = data.to(device), label_segnum.to(device), label_color.to(device)
+            logits_segnum, logits_color = net(data)
+            loss_segnum = criterion(logits_segnum, label_segnum)
+            loss_color = criterion(logits_color, label_color)
+            loss = loss_segnum + loss_color
+            val_loss += loss.item()
+            val_loss_segnum += loss_segnum.item()
+            val_loss_color +=loss_color.item()
+            preds_segnum = logits_segnum.max(dim=1)[1]
+            preds_color = logits_color.max(dim=1)[1]
+
+            total += label_segnum.size(0)
+            correct_segnum += preds_segnum.eq(label_segnum).sum().item()
+            correct_color += preds_color.eq(label_color).sum().item()
+
+            t_range.set_postfix({'Val loss':(val_loss / (batch_idx + 1)),
+                                 "acc_segnum": correct_segnum/total,
+                                 "acc_color": correct_color/total,
+                                 })
+    time_cost = int((datetime.datetime.now() - time_cost).total_seconds())
+    print(f"preds_segnum is: {preds_segnum}")
+    print(f"label_segnum is: {label_segnum}")
+    print(f"preds_color is: {preds_color}")
+    print(f"label_color is: {label_color}")
+    return {
+        "loss": float("%.3f" % (val_loss / (batch_idx + 1))),
+        "loss_segnum": float("%.3f" % (val_loss_segnum / (batch_idx + 1))),
+        "loss_color": float("%.3f" % (val_loss_color / (batch_idx + 1))),
+        "acc_segnum": float("%.3f" % (100. * correct_segnum/total)),
+        "acc_color": float("%.3f" % (100. * correct_color/total)),
+        "time": time_cost
+    }
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
