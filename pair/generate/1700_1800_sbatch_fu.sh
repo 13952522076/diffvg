cf59db5dc32d809e90a34af4306a460a97319b79
Xu Ma <xuma@my.unt.edu>
2021-11-04 20:33:36 -0400

    update

--- pair/generate/1700_1800_sbatch_fu.sh
+++ pair/generate/1700_1800_sbatch_fu.sh
@@ -15,4 +15,4 @@ export CXX=/shared/centos7/gcc/8.1.0/bin/g++
 source activate im2vec
 
 cd /scratch/ma.xu1/diffvg/pair/generate
-python generate_pair.py  --pool_size 60  --free --start 1600 --end 1700
+python generate_pair.py  --pool_size 60  --free --start 1700 --end 1800
--- pair/generate/800_900_sbatch_multi.sh
+++ pair/generate/800_900_sbatch_multi.sh
@@ -17,4 +17,4 @@ export CXX=/shared/centos7/gcc/8.1.0/bin/g++
 source activate im2vec
 
 cd /scratch/ma.xu1/diffvg/pair/generate
-python generate_pair.py  --pool_size 60  --free --start 400 --end 500
+python generate_pair.py  --pool_size 60  --free --start 800 --end 900
