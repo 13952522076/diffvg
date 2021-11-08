ddd6088058b2943446342fa95724bb0fc53e9adb
Xu Ma <xuma@my.unt.edu>
2021-11-08 01:52:09 -0500

    Update main.py

--- VAE_MNIST/main.py
+++ VAE_MNIST/main.py
@@ -6,23 +6,18 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-import warnings
-warnings.filterwarnings("ignore")
-
-
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--hidden_dim', type=int, default=256)
-parser.add_argument('--epochs', type=int, default=70, metavar='N',
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=50, metavar='N',
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -42,49 +37,18 @@ test_loader = torch.utils.data.DataLoader(
 
 
 class VAE(nn.Module):
-    def __init__(self, hidden_dim=256):
+    def __init__(self):
         super(VAE, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(784, 512),
-            nn.BatchNorm1d(512),
-            nn.LeakyReLU(),
-            nn.Linear(512, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(),
-            nn.Linear(256, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU()
-        )
-        self.mu = nn.Sequential(
-            nn.Linear(256, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(),
-            nn.Linear(256, hidden_dim)
-        )
-        self.log_var = nn.Sequential(
-            nn.Linear(256, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(),
-            nn.Linear(256, hidden_dim)
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(hidden_dim, 256),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(),
-            nn.Linear(256, 512),
-            nn.BatchNorm1d(512),
-            nn.LeakyReLU(),
-            nn.Linear(512, 512),
-            nn.BatchNorm1d(512),
-            nn.LeakyReLU(),
-            nn.Linear(512, 784)
-        )
 
-
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 64)
+        self.fc22 = nn.Linear(400, 64)
+        self.fc3 = nn.Linear(64, 400)
+        self.fc4 = nn.Linear(400, 784)
 
     def encode(self, x):
-        h = self.encoder(x)
-        return self.mu(h), self.log_var(h)
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -92,7 +56,8 @@ class VAE(nn.Module):
         return mu + eps*std
 
     def decode(self, z):
-        return torch.sigmoid(self.decoder(z))
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, 784))
@@ -100,46 +65,43 @@ class VAE(nn.Module):
         return self.decode(z), mu, logvar
 
 
-model = VAE(hidden_dim=args.hidden_dim).to(device)
+model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.7)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.7)
 
-best_loss = 999999.
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return BCE + KLD, BCE, KLD
+
+    return BCE + KLD
 
 
 def train(epoch):
     model.train()
     train_loss = 0
-    bce_loss = 0
-    kld_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss, loss_bce, loss_kld = loss_function(recon_batch, data, mu, logvar)
+        loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
-        bce_loss +=loss_bce.item()
-        kld_loss +=loss_kld.item()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tBCE-Loss: {:.6f}\tKLD-Loss: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
-                train_loss / (batch_idx+1.0),
-                bce_loss / (batch_idx+1.0),
-                kld_loss / (batch_idx+1.0)
-            ))
-    return train_loss / (batch_idx+1.0)
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
 
 def test(epoch):
     model.eval()
@@ -148,8 +110,7 @@ def test(epoch):
         for i, (data, _) in enumerate(test_loader):
             data = data.to(device)
             recon_batch, mu, logvar = model(data)
-            loss_all, loss_bce, loss_kld = loss_function(recon_batch, data, mu, logvar)
-            test_loss += loss_all.item()
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
@@ -159,17 +120,15 @@ def test(epoch):
 
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
-    return test_loss
 
 if __name__ == "__main__":
     for epoch in range(1, args.epochs + 1):
-        train_loss = train(epoch)
-        test_loss = test(epoch)
+        train(epoch)
+        test(epoch)
+        torch.save(model.state_dict(),"last.pth")
         scheduler.step()
-        if best_loss > test_loss:
-            torch.save(model.state_dict(),f"best_dim{args.hidden_dim}.pth"),
         with torch.no_grad():
-            sample = torch.randn(64, args.hidden_dim).to(device)
+            sample = torch.randn(64, 64).to(device)
             sample = model.decode(sample).cpu()
             save_image(sample.view(64, 1, 28, 28),
                        'results/sample_' + str(epoch) + '.png')
