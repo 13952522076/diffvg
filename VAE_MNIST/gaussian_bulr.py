05805640950ec6f5c06e43a1a42dc0acc8737f7f
Xu Ma <xuma@my.unt.edu>
2021-11-09 12:13:47 -0500

    Update gaussian_bulr.py

--- VAE_MNIST/gaussian_bulr.py
+++ VAE_MNIST/gaussian_bulr.py
@@ -8,6 +8,6 @@ src = cv2.imread('hr/id1_id4_0.2.png', cv2.IMREAD_UNCHANGED)
 dst = cv2.GaussianBlur(src,(5,5),cv2.BORDER_DEFAULT)
 
 # display input and output image
-cv2.imwrite("Gaussian Smoothing",numpy.hstack((src, dst)))
+cv2.imwrite("blur.png",numpy.hstack((src, dst)))
 # cv2.waitKey(0) # waits until a key is pressed
 # cv2.destroyAllWindows() # d
