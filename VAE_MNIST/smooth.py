2afc3d3c28a87146cfd2ac61fe893730b2cf074d
Xu Ma <xuma@my.unt.edu>
2021-11-09 12:47:03 -0500

    update

---
+++ VAE_MNIST/smooth.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import argparse
+import torch
+# import cv2
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from PIL import Image
+import torchvision.transforms.functional as TF
+import os
+try:
+    os.mkdir("hr_binary")
+except:
+    pass
+filename = "id1_id4_0.9.png"
+image = Image.open(f'hr/{filename}')
+x = TF.to_tensor(image)
+x.unsqueeze_(0)
+x[x>=0.5] = 1.0
+x[x<0.5] = 0.0
+# x = TF.resize(x, 56)
+x = TF.resize(x,224, interpolation=TF.InterpolationMode.BILINEAR)
+
+save_image(x, f'hr_binary/{filename}')
+
+
+
+print(x.shape)
