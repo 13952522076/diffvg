b7d43cf92c0321acd1618c06103753920d465e8a
Xu Ma <xuma@my.unt.edu>
2021-11-09 17:08:49 -0500

    update

--- pair/Layerwise/main.py
+++ pair/Layerwise/main.py
@@ -62,25 +62,12 @@ def get_bezier_circle(radius=1, segments=4, bias=None):
     points = []
     if bias is None:
         bias = (random.random(), random.random())
-    m_point = (1.0, 0.0)
-    points.append(m_point)
-    m1_point_angle = np.degrees(4/3*np.tan(np.pi/(2*segments)))
-    m1_point_radius = np.sqrt(1+ ( 4/3*np.tan(np.pi/(2*segments)))**2)
-    m3_point_angle = 360/segments
-    for i in range(0, segments):
-        m1_point = (m1_point_radius*np.cos(np.deg2rad(i*m3_point_angle+m1_point_angle)),
-                    m1_point_radius*np.sin(np.deg2rad(i*m3_point_angle+m1_point_angle)))
-        m2_point_angle = m3_point_angle-m1_point_angle
-        m2_point = (m1_point_radius*np.cos(np.deg2rad(i*m3_point_angle+m2_point_angle)),
-                    m1_point_radius*np.sin(np.deg2rad(i*m3_point_angle+m2_point_angle)))
-        m3_point = (np.cos(np.deg2rad(i*m3_point_angle+m3_point_angle)),
-                    np.sin(np.deg2rad(i*m3_point_angle+m3_point_angle)))
-        points.append(m1_point)
-        points.append(m2_point)
-        points.append(m3_point)
-    points.reverse()
+    avg_degree = 360 / (segments*3)
+    for i in range(0, segments*3):
+        point = (np.cos(np.deg2rad(i * avg_degree)),
+                    np.sin(np.deg2rad(i * avg_degree)))
+        points.append(point)
     points = torch.tensor(points)
-    points = points[:-1,:]
     points = (points)*radius + torch.tensor(bias).unsqueeze(dim=0)
     points = points.type(torch.FloatTensor)
     return points
